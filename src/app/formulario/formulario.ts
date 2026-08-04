50b93b394a3ae5896f3253ba82030461cc2769ab
Alisson Freire <alisson.batista@docente.senai.br>
2026-08-04 16:50:37 -0300

    Importado o PessoaService

--- src/app/formulario/formulario.ts
+++ src/app/formulario/formulario.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Pessoa } from '../model/pessoa';
+import { PessoaService } from '../service/pessoa-service';
 
 @Component({
   selector: 'app-formulario',
@@ -6,4 +8,25 @@ import { Component } from '@angular/core';
   templateUrl: './formulario.html',
   styleUrl: './formulario.css',
 })
-export class Formulario {}
+export class Formulario {
+
+  //Pessoa p = new Pessoa()
+  id = 0
+  nome = ''
+  email = ''
+  cpf = 0.0
+  dataNascimento = ''
+
+
+  constructor(private pessoaService: PessoaService){}
+
+  salvar(){
+    this.pessoaService.adicionar({
+      id: 0,
+      nome : this.nome,
+      email : this.email,
+      cpf : this.cpf,
+      dataNascimento : this.dataNascimento
+    })
+  }
+}
