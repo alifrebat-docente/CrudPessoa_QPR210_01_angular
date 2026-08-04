47c71bacdbc11a9b575336fccf901326675e1e45
Alisson Freire <alisson.batista@docente.senai.br>
2026-08-04 14:32:26 -0300

    Importado o componente Formulario

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Menu } from './menu/menu';
+import { Formulario } from './formulario/formulario'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Menu],
+  imports: [RouterOutlet, Menu, Formulario],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
