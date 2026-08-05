46e0f80de60caad743b336944075db02bc7089a4
Alisson Freire <alisson.batista@docente.senai.br>
2026-08-05 16:53:52 -0300

    Ajustado as rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,26 @@
 import { Routes } from '@angular/router';
 
-import { Formulario } from './formulario/formulario';
-import { Lista } from './lista/lista';
+import { Formulario } from './component/formulario/formulario';
+import { Lista } from './component/lista/lista';
+import { HomeComponent } from './component/home-component/home-component';
 
 export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent
+    },
     {
         path: 'cadastro',
         component: Formulario
     },
-
     {
         path: 'lista',
         component: Lista
     },
-   
+
 ];
