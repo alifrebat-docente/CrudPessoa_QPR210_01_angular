4c57488d3ee371795dfe6bc690358678e2a27088
Alisson Freire <alisson.batista@docente.senai.br>
2026-08-05 16:47:38 -0300

    Criado a pasta e os serviço Pessoa

---
+++ src/app/services/pessoa-service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Pessoa } from '../models/pessoa';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class PessoaService {
+    private pessoas: Pessoa[] = []
+
+    adicionar(pessoa: Pessoa) {
+        this.pessoas.push(pessoa)
+        localStorage.setItem('pessoasStorage', JSON.stringify(this.pessoas))
+    }
+
+    listar() {
+        const dados = localStorage.getItem('pessoasStorage');
+
+        if (dados) {
+            this.pessoas = JSON.parse(dados);
+        } else {
+            this.pessoas = [];
+        }
+
+        return this.pessoas;
+    }
+
+
+}
